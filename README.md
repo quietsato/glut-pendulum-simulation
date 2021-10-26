10a80c5b29de0cb1e76a819e723d832bd3635216
再提出 <quietsato@gmail.com>
2021-10-26 13:53:26 +0900

    Initialise project

---
+++ README.md
@@ -0,0 +1,10 @@
+# GLUT Pendulum Simulation
+
+## Build & Run
+
+```sh
+cmake -B build
+cd ./build/
+make
+./pendulum.out
+```
---
+++ src/main.cpp
@@ -0,0 +1,73 @@
+#include <cmath>
+#include <iostream>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+void init() {
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glEnable(GL_LINE_SMOOTH);
+}
+
+void drawCeiling() {
+    glBegin(GL_LINES);
+    glVertex2d(-1.0, 1.0);
+    glVertex2d(1.0, 1.0);
+    glEnd();
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glLoadIdentity();
+    glColor3d(1, 1, 1);
+
+    drawCeiling();
+
+    glBegin(GL_LINES);
+
+    glVertex2d(0.0, 1.0);
+    glVertex2d(0.0, 0.0);
+
+    glEnd();
+
+    glutSwapBuffers();
+}
+
+void centreViewport(int width, int height) {
+    int window_size = std::min(width, height);
+
+    int x0 = (width - window_size) / 2;
+    int y0 = (height - window_size) / 2;
+
+    glViewport(x0, y0, window_size, window_size);
+}
+
+void reshape(int width, int height) {
+    centreViewport(width, height);
+
+    glLoadIdentity();
+    gluPerspective(30.0, (double)width / (double)height, 1.0, 100.0);
+    glTranslated(0.0, 0.0, -5.0);
+    gluLookAt(3.0, 4.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+}
+
+int main(int argc, char *argv[]) {
+    glutInit(&argc, argv);
+
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+
+    glutCreateWindow(argv[0]);
+
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+
+    init();
+
+    glutMainLoop();
+
+    return 0;
+}
