90f46455046ba47479c00ab9aab27b8b58d7517c
quietsato <quietsato@gmail.com>
2021-11-16 21:41:11 +0900

    Pack to class

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 
-#include "functions.hpp"
+#include "pendulum/pendulum.hpp"
 
 #ifdef __APPLE__
 #include <GLUT/glut.h>
@@ -10,23 +10,47 @@
 #include <GL/glut.h>
 #endif
 
-#define INF 1000000
-#define HISTORY_MAX 1000
-
-double g = 9.8;
-double m1 = 1.0;
-double l1 = 0.5;
-double m2 = 0.5;
-double l2 = 0.25;
-
-std::vector<std::pair<double, double>> history;
-size_t history_index = 0;
+DoublePendulum *p1;
+DoublePendulum *p2;
+DoublePendulum *p3;
+DoublePendulum *p4;
 
 void init() {
     glClearColor(0.0, 0.0, 0.0, 1.0);
     glEnable(GL_LINE_SMOOTH);
+
+    p1 = new DoublePendulum(                                      //
+        /* m1 = */ 1.0, /* m2 = */ 0.5,                           //
+        /* l1 = */ 0.5, /* l2 = */ 0.25,                          //
+        /* theta1 = */ M_PI * 2 / 3, /* theta2 = */ M_PI * 2 / 3, //
+        /* omega1 = */ 0, /* omega2 = */ 0                        //
+    );
+    p2 = new DoublePendulum(                                              //
+        /* m1 = */ 1.0, /* m2 = */ 0.5,                                   //
+        /* l1 = */ 0.5, /* l2 = */ 0.25,                                  //
+        /* theta1 = */ M_PI * 2 / 3 + 0.001, /* theta2 = */ M_PI * 2 / 3, //
+        /* omega1 = */ 0, /* omega2 = */ 0                                //
+    );
+    p2->setPendulumColor(1, 0, 0);
+    p3 = new DoublePendulum(                                               //
+        /* m1 = */ 1.0, /* m2 = */ 0.5,                                    //
+        /* l1 = */ 0.5, /* l2 = */ 0.25,                                   //
+        /* theta1 = */ M_PI * 2 / 3 + 0.0001, /* theta2 = */ M_PI * 2 / 3, //
+        /* omega1 = */ 0, /* omega2 = */ 0                                 //
+    );
+    p3->setPendulumColor(0, 1, 0);
+    p4 = new DoublePendulum(                                                //
+        /* m1 = */ 1.0, /* m2 = */ 0.5,                                     //
+        /* l1 = */ 0.5, /* l2 = */ 0.25,                                    //
+        /* theta1 = */ M_PI * 2 / 3 + 0.00001, /* theta2 = */ M_PI * 2 / 3, //
+        /* omega1 = */ 0, /* omega2 = */ 0                                  //
+    );
+    p4->setPendulumColor(0, 0, 1);
 }
 
+//
+// Display
+//
 void drawLattice() {
     glColor3d(0.2, 0.2, 0.2);
     glBegin(GL_LINES);
@@ -37,124 +61,38 @@ void drawLattice() {
     glEnd();
 }
 
-double h = 0.005;
+void drawPendulum() {
+    p1->step();
+    p2->step();
+    p3->step();
+    p4->step();
 
-double theta1 = 2 * M_PI / 3;
-double omega1 = 0;
-double theta2 = 2 * M_PI / 3;
-double omega2 = 0;
+    p4->display();
+    p3->display();
+    p2->display();
+    p1->display();
+}
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
-
     glLoadIdentity();
 
     drawLattice();
-
-    // runge-kutta
-    double k1_theta1 = h * theta_dot1(omega1);
-    double k1_theta2 = h * theta_dot2(omega2);
-    double k1_omega_dot1 =
-        omega_dot1(g, m1, m2, l1, l2, theta1, theta2, omega1, omega2);
-    double k1_omega_dot2 =
-        omega_dot2(g, m1, m2, l1, l2, theta1, theta2, omega1, omega2);
-    double k1_omega1 = h * k1_omega_dot1;
-    double k1_omega2 = h * k1_omega_dot2;
-
-    double k2_theta1 = h * theta_dot1(omega1 + k1_omega1 / 2);
-    double k2_theta2 = h * theta_dot2(omega2 + k1_omega2 / 2);
-    double k2_omega_dot1 = omega_dot1(
-        g, m1, m2, l1, l2, theta1 + k1_theta1 / 2, theta2 + k1_theta2 / 2,
-        omega1 + k1_omega1 / 2, omega2 + k1_omega2 / 2);
-    double k2_omega_dot2 = omega_dot2(
-        g, m1, m2, l1, l2, theta1 + k1_theta1 / 2, theta2 + k1_theta2 / 2,
-        omega1 + k1_omega1 / 2, omega2 + k1_omega2 / 2);
-    double k2_omega1 = h * k2_omega_dot1;
-    double k2_omega2 = h * k2_omega_dot2;
-
-    double k3_theta1 = h * theta_dot1(omega1 + k2_omega1 / 2);
-    double k3_theta2 = h * theta_dot2(omega2 + k2_omega2 / 2);
-    double k3_omega_dot1 = omega_dot1(
-        g, m1, m2, l1, l2, theta1 + k2_theta1 / 2, theta2 + k2_theta2 / 2,
-        omega1 + k2_omega1 / 2, omega2 + k2_omega2 / 2);
-    double k3_omega_dot2 = omega_dot2(
-        g, m1, m2, l1, l2, theta1 + k2_theta1 / 2, theta2 + k2_theta2 / 2,
-        omega1 + k2_omega1 / 2, omega2 + k2_omega2 / 2);
-    double k3_omega1 = h * k3_omega_dot1;
-    double k3_omega2 = h * k3_omega_dot2;
-
-    double k4_theta1 = h * theta_dot1(omega1 + k3_omega1);
-    double k4_theta2 = h * theta_dot2(omega2 + k3_omega2);
-    double k4_omega_dot1 =
-        omega_dot1(g, m1, m2, l1, l2, theta1 + k3_theta1, theta2 + k3_theta2,
-                   omega1 + k3_omega1, omega2 + k3_omega2);
-    double k4_omega_dot2 =
-        omega_dot2(g, m1, m2, l1, l2, theta1 + k3_theta1, theta2 + k3_theta2,
-                   omega1 + k3_omega1, omega2 + k3_omega2);
-    double k4_omega1 = h * k4_omega_dot1;
-    double k4_omega2 = h * k4_omega_dot2;
-
-    theta1 += (k1_theta1 + 2.0 * k2_theta1 + 2.0 * k3_theta1 + k4_theta1) / 6.0;
-    omega1 += (k1_omega1 + 2.0 * k2_omega1 + 2.0 * k3_omega1 + k4_omega1) / 6.0;
-    theta2 += (k1_theta2 + 2.0 * k2_theta2 + 2.0 * k3_theta2 + k4_theta2) / 6.0;
-    omega2 += (k1_omega2 + 2.0 * k2_omega2 + 2.0 * k3_omega2 + k4_omega2) / 6.0;
-
-    double x1 = l1 * sin(theta1), y1 = -l1 * cos(theta1);
-    double x2 = x1 + l2 * sin(theta2), y2 = y1 - l2 * cos(theta2);
-
-    glBegin(GL_LINES);
-
-    glColor3d(1, 1, 1);
-
-    glVertex2d(0.0, 0.0);
-    glVertex2d(x1, y1);
-    glVertex2d(x1, y1);
-    glVertex2d(x2, y2);
-
-    glEnd();
-
-    if (history.size() < HISTORY_MAX) {
-        history.push_back(std::pair<double, double>(x2, y2));
-
-        glBegin(GL_LINES);
-        glColor3d(1, 0, 0);
-        for (size_t i = 0; i < history.size() - 1; i++) {
-            glVertex2d(history[i].first, history[i].second);
-            glVertex2d(history[i + 1].first, history[i + 1].second);
-        }
-        glEnd();
-    } else {
-        history[history_index] = std::pair<double, double>(x2, y2);
-
-        glBegin(GL_LINES);
-        glColor3d(1, 0, 0);
-        for (size_t j = 0; j < HISTORY_MAX - 1; j++) {
-            size_t i1 = (history_index + j + 1) % HISTORY_MAX;
-            size_t i2 = (i1 + 1) % HISTORY_MAX;
-            glVertex2d(history[i1].first, history[i1].second);
-            glVertex2d(history[i2].first, history[i2].second);
-        }
-        glEnd();
-
-        history_index = (history_index + 1) % HISTORY_MAX;
-    }
-
-    // printf("%lf %lf\n", theta1, theta2);
+    drawPendulum();
 
     glutSwapBuffers();
 }
 
-void centreViewport(int width, int height) {
-    int window_size = std::min(width, height);
-
-    int x0 = (width - window_size) / 2;
-    int y0 = (height - window_size) / 2;
-
-    glViewport(x0, y0, window_size, window_size);
-}
-
 void reshape(int width, int height) {
-    centreViewport(width, height);
+    // Center viewport
+    {
+        int window_size = std::min(width, height);
+
+        int x0 = (width - window_size) / 2;
+        int y0 = (height - window_size) / 2;
+
+        glViewport(x0, y0, window_size, window_size);
+    }
     glLoadIdentity();
 }
 
@@ -170,9 +108,7 @@ void mouse(int button, int state, int x, int y) {
     }
 }
 
-void startupTimeout(int _value) {
-    glutIdleFunc(idle);
-}
+void startupTimeout(int _value) { glutIdleFunc(idle); }
 
 int main(int argc, char *argv[]) {
     glutInit(&argc, argv);
---
+++ src/pendulum/pendulum.cpp
@@ -0,0 +1,155 @@
+#include "pendulum.hpp"
+
+#include <cmath>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+double omega_dot1(double g, double m1, double m2, double l1, double l2,
+                  double theta1, double theta2, double omega1, double omega2) {
+    double C = cos(theta1 - theta2);
+    double S = sin(theta1 - theta2), s1 = sin(theta1), s2 = sin(theta2);
+    double M = m1 + m2;
+    return ((g * (s2 * C - (M / m2) * s1)                                //
+             - (S * (l1 * omega1 * omega1 * C + l2 * omega2 * omega2)))) //
+           /                                                             //
+           (l1 * ((M / m2) - C * C));                                    //
+}
+
+double omega_dot2(double g, double m1, double m2, double l1, double l2,
+                  double theta1, double theta2, double omega1, double omega2) {
+    double C = cos(theta1 - theta2);
+    double S = sin(theta1 - theta2), s1 = sin(theta1), s2 = sin(theta2);
+    double M = m1 + m2;
+    return ((g * (M / m2) * (s1 * C - s2)                                     //
+             + S * ((M / m2) * l1 * omega1 * omega1 + l2 * omega2 * omega2))) //
+           /                                                                  //
+           (l2 * ((M / m2) - C * C));                                         //
+}
+
+double theta_dot1(double omega1) { return omega1; }
+double theta_dot2(double omega2) { return omega2; }
+
+void DoublePendulum::recordLocus(double x2, double y2) {
+    if (this->locus.size() < HISTORY_MAX) {
+        this->locus.push_back(std::pair<double, double>(x2, y2));
+        if (this->isEnabledLocus) {
+            glBegin(GL_LINES);
+            glColor3d(1, 0, 0);
+            for (size_t i = 0; i < this->locus.size() - 1; i++) {
+                glVertex2d(this->locus[i].first, this->locus[i].second);
+                glVertex2d(this->locus[i + 1].first, this->locus[i + 1].second);
+            }
+            glEnd();
+        }
+    } else {
+        this->locus[this->locusIndex] = std::pair<double, double>(x2, y2);
+        if (this->isEnabledLocus) {
+            glBegin(GL_LINES);
+            glColor3d(1, 0, 0);
+            for (size_t j = 0; j < HISTORY_MAX - 1; j++) {
+                size_t i1 = (this->locusIndex + j + 1) % HISTORY_MAX;
+                size_t i2 = (i1 + 1) % HISTORY_MAX;
+                glVertex2d(this->locus[i1].first, this->locus[i1].second);
+                glVertex2d(this->locus[i2].first, this->locus[i2].second);
+            }
+            glEnd();
+        }
+        this->locusIndex = (this->locusIndex + 1) % HISTORY_MAX;
+    }
+}
+
+DoublePendulum::DoublePendulum(                                //
+    double m1, double m2, double l1, double l2,                //
+    double theta1, double theta2, double omega1, double omega2 //
+) {
+    this->m1 = m1;
+    this->m2 = m2;
+    this->l1 = l1;
+    this->l2 = l2;
+    this->theta1 = theta1;
+    this->theta2 = theta2;
+    this->omega1 = omega1;
+    this->omega2 = omega2;
+}
+
+void DoublePendulum::display() {
+    double x1 = l1 * sin(theta1), y1 = -l1 * cos(theta1);
+    double x2 = x1 + l2 * sin(theta2), y2 = y1 - l2 * cos(theta2);
+
+    glBegin(GL_LINES);
+    glColor3d(this->pr, this->pg, this->pb);
+
+    glVertex2d(0.0, 0.0);
+    glVertex2d(x1, y1);
+    glVertex2d(x1, y1);
+    glVertex2d(x2, y2);
+
+    glEnd();
+}
+
+void DoublePendulum::step() {
+    double k1_theta1 = h * theta_dot1(omega1);
+    double k1_theta2 = h * theta_dot2(omega2);
+    double k1_omega1 =                                            //
+        h * omega_dot1(                                           //
+                g, m1, m2, l1, l2, theta1, theta2, omega1, omega2 //
+            );
+    double k1_omega2 =                                            //
+        h * omega_dot2(                                           //
+                g, m1, m2, l1, l2, theta1, theta2, omega1, omega2 //
+            );
+
+    double k2_theta1 = h * theta_dot1(omega1 + k1_omega1 / 2);
+    double k2_theta2 = h * theta_dot2(omega2 + k1_omega2 / 2);
+    double k2_omega1 =
+        h * omega_dot1(                                         //
+                g, m1, m2, l1, l2,                              //
+                theta1 + k1_theta1 / 2, theta2 + k1_theta2 / 2, //
+                omega1 + k1_omega1 / 2, omega2 + k1_omega2 / 2  //
+            );
+    double k2_omega2 =                                          //
+        h * omega_dot2(                                         //
+                g, m1, m2, l1, l2,                              //
+                theta1 + k1_theta1 / 2, theta2 + k1_theta2 / 2, //
+                omega1 + k1_omega1 / 2, omega2 + k1_omega2 / 2  //
+            );
+
+    double k3_theta1 = h * theta_dot1(omega1 + k2_omega1 / 2);
+    double k3_theta2 = h * theta_dot2(omega2 + k2_omega2 / 2);
+    double k3_omega1 =                                          //
+        h * omega_dot1(                                         //
+                g, m1, m2, l1, l2,                              //
+                theta1 + k2_theta1 / 2, theta2 + k2_theta2 / 2, //
+                omega1 + k2_omega1 / 2, omega2 + k2_omega2 / 2  //
+            );
+    double k3_omega2 =                                          //
+        h * omega_dot2(                                         //
+                g, m1, m2, l1, l2,                              //
+                theta1 + k2_theta1 / 2, theta2 + k2_theta2 / 2, //
+                omega1 + k2_omega1 / 2, omega2 + k2_omega2 / 2  //
+            );
+
+    double k4_theta1 = h * theta_dot1(omega1 + k3_omega1);
+    double k4_theta2 = h * theta_dot2(omega2 + k3_omega2);
+    double k4_omega1 =                                  //
+        h * omega_dot1(                                 //
+                g, m1, m2, l1, l2,                      //
+                theta1 + k3_theta1, theta2 + k3_theta2, //
+                omega1 + k3_omega1, omega2 + k3_omega2  //
+            );
+    double k4_omega2 =                                  //
+        h * omega_dot2(                                 //
+                g, m1, m2, l1, l2,                      //
+                theta1 + k3_theta1, theta2 + k3_theta2, //
+                omega1 + k3_omega1, omega2 + k3_omega2  //
+            );
+
+    theta1 += (k1_theta1 + 2.0 * k2_theta1 + 2.0 * k3_theta1 + k4_theta1) / 6.0;
+    omega1 += (k1_omega1 + 2.0 * k2_omega1 + 2.0 * k3_omega1 + k4_omega1) / 6.0;
+    theta2 += (k1_theta2 + 2.0 * k2_theta2 + 2.0 * k3_theta2 + k4_theta2) / 6.0;
+    omega2 += (k1_omega2 + 2.0 * k2_omega2 + 2.0 * k3_omega2 + k4_omega2) / 6.0;
+}
