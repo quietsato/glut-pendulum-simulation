d7dc716c0167aa46470bcc6c71b591d81c9617ca
quietsato <quietsato@gmail.com>
2021-12-07 09:55:58 +0900

    Load config from yaml files

--- src/config_loader/config_loader.hpp
+++ src/config_loader/config_loader.hpp
@@ -2,4 +2,20 @@
 
 #include "../../lib/yaml-cpp/include/yaml-cpp/yaml.h"
 
-void load();
+#include <string>
+#include <vector>
+
+typedef struct PendulumConfig {
+    double m1;
+    double m2;
+    double l1;
+    double l2;
+    double theta1_deg;
+    double theta2_deg;
+    double omega1_deg;
+    double omega2_deg;
+} PendulumConfig;
+
+namespace config_loader {
+std::vector<PendulumConfig> load(std::string path);
+}
